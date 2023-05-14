2ea0efbd8210cf0c477c575af589c9f05776c1b0
Nicolas Goy <kuon@goyman.com>
2023-05-14 22:59:12 +0200

    Reduce registers.zig

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,6 @@ const microzig = @import("microzig");
 const regs = microzig.chip.peripherals;
 
 pub fn main() !void {
-    var addr = @ptrToInt(&regs.SPI1.DR.raw);
+    var addr = @ptrToInt(&regs.AES.CR.raw);
     _ = addr;
 }
